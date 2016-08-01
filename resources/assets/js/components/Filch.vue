955957d323fe247b47c4fdeb6155d5251a5c8fcd
Dustin Woodard <dustinwoodard@weber.edu>
2016-08-01 16:58:14 -0600

    edit to Filch.vue

--- resources/assets/js/components/Filch.vue
+++ resources/assets/js/components/Filch.vue
@@ -1,20 +1,27 @@
 <template>
     <div class="Filch">
-        <p>Filch
-            <slot></slot>
-        </p>
+         Filch
+            <slot></slot> 
+
     </div>
 </template>
 
 <script>
     export default {
-         
+         data:{
+             message: function(){
+                 return 'this is a default message';
+             }
+         }
     }
 </script>
 
 <style>
   .Filch{
-      border: 1px solid red;
+      border: 1px solid #2526ff; 
       padding: 10px;
+      margin-bottom:1vh;
+      height:10vh;
+      line-height:10vh;
   }
 </style>
--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -2,12 +2,10 @@ import Vue from 'Vue';
 import Filch from './components/Filch.vue';
 
 
-// var app = require('./app');
+// var app = require('./app'); is now hard coded into Vue({})
 
 new Vue({
     el: '#app',
-    data: {
-        message: "this is a message"
-    },
+    data: { message: "this is a message"},
     components: {Filch}
 });
