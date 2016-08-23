da48f93963535236f914bd50570687e72d3293f3
Dustin Woodard <dustinwoodard@weber.edu>
2016-08-23 15:14:52 -0600

    gulpfile.js update

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,5 @@ var elixir = require("laravel-elixir");
 require('laravel-elixir-vueify');
 
 elixir(function (mix) {
-    mix.browserify("main.js", 'public/js/test.js');
     mix.browserify("main.js", 'src/filch/filch.js');
 });
